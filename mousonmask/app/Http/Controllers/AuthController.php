156aee262100bc2340c716fc455cf9208780a785
Christy <118579935+chrstyy@users.noreply.github.com>
2023-12-27 11:13:44 +0700

    signup

--- mousonmask/app/Http/Controllers/AuthController.php
+++ mousonmask/app/Http/Controllers/AuthController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function signup(Request $request)
+    {
+
+        return redirect()->route('signup');
+    }
 }
